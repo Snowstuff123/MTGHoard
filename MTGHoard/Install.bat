c94f76e6309a4bd021d28608f7c8f95e2d636bab
John Heisler <108553174+Snowstuff123@users.noreply.github.com>
2023-10-04 22:05:36 -0400

    Added Main Files
    YAY (again)

---
+++ MTGHoard/Install.bat
@@ -0,0 +1,7 @@
+﻿@echo OFF
+@echo Installing MTG Hoard by Sean Tarr & John Heisler
+xcopy .\MTGHoard.txt %appdata%\MTGHoard\MTGHoard.ps1* /Q /Y
+xcopy .\icon.png %appdata%\MTGHoard\ /Q /Y
+xcopy .\MTGHoard.lnk %appdata%\MTGHoard\MTGHoard.lnk* /Q /Y
+@echo Complete!
+Pause
